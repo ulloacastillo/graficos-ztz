e95f1d2f06de78004c8f1fbee17863c82f5bceba
Felipe Lopez Araya <felipejavierlopezaraya@gmail.com>
2024-02-15 15:43:59 -0300

    show camera icon in button

--- components/App.jsx
+++ components/App.jsx
@@ -83,11 +83,11 @@ const App = () => {
             justifyContent: 'center',
             alignItems: 'center',
           }}
-          className={`absolute bottom-0 left-0 m-5 p-2 cursor-pointer transition-all bg-ztz-mainblue text-white px-6 py-2 rounded-lg
+          className={`absolute bottom-0 left-0 m-5 p-2 cursor-pointer transition-all bg-ztz-mainblue text-white px-4 py-1 rounded-lg
 border-ztz-indigoblue
 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
 active:border-b-[2px] active:brightness-90 active:translate-y-[2px]${
-            !open && 'w-16'
+            !open && 'w-20'
           }`}
         >
           {open ? (
