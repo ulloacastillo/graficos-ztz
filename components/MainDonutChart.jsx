823506db12dd680ff8d6987aef1b3545dbe1b3fe
Jorge Ulloa Castillo <jorgeulloacastillo@gmail.com>
2024-02-15 15:51:27 -0300

    add click to donut chart claims

--- components/MainDonutChart.jsx
+++ components/MainDonutChart.jsx
@@ -10,15 +10,15 @@ function DonutChart() {
   const endColor = useChartSettings((state) => state.endColor);
   const textColor = useChartSettings((state) => state.textColor);
   const chartConfig = useRef();
-
-  let data = useSelector((state) => state.chartData);
+  let clickedBar = { id: null, color: null };
+  const data = useSelector((state) => state.chartData);
   const width = 800;
   const height = 420;
   const radius = Math.min(width, height) / 2;
 
   const theme = useChartSettings((state) => state.theme);
 
-  data = [...data].sort((a, b) => {
+  const dataSorted = [...data].sort((a, b) => {
     const monthA = parseInt(a[0], 10);
     const monthB = parseInt(b[0], 10);
     return monthA - monthB;
@@ -37,19 +37,19 @@ function DonutChart() {
 
     const colorScale = d3
       .scaleLinear()
-      .domain([0, data.length - 1])
+      .domain([0, dataSorted.length - 1])
       .range([initialColor, endColor]);
 
     const pie = d3.pie().value((d) => d[1]);
-    const arcs = pie(data);
+    const arcs = pie(dataSorted);
 
+    console.log(arcs);
     const arc = d3
       .arc()
       .innerRadius(radius * 0.5)
       .outerRadius(radius * 0.8);
 
-    const outerArc = d3
-      .arc()
+    d3.arc()
       .innerRadius(radius * 0.9)
       .outerRadius(radius * 0.9);
 
@@ -76,24 +76,103 @@ function DonutChart() {
       .data(arcs)
       .enter()
       .append('text')
+      .attr('id', (d, i) => `mybar${i}`)
+      .on('click', (e, d) => {
+        const selectedBar = d3.select(e.currentTarget);
+        const clickedId = selectedBar.attr('id');
+        console.log(d);
+        let claimsPreviousMonth;
+
+        if (clickedId !== 'mybar0') {
+          const idPrevious = parseInt(clickedId.slice(5)) - 1;
+          //const previousMonth = d3.select(`#mybar${idPrevious}`);
+
+          claimsPreviousMonth =
+            arcs[parseInt(clickedId.substring(5)) - 1].data[1];
+        }
+
+        const receivedText = d3.select('#received');
+        const increaseText = d3.select('#increase');
+        const upOrDown = d3.select('#upOrDown');
+        if (clickedBar.id === clickedId) {
+          d3.select('#' + clickedBar.id)
+            .attr('fill', clickedBar.color)
+            .attr('stroke', clickedBar.color)
+            .attr('stroke-width', 0.3);
+          receivedText.html(
+            `<div id="received" className="text-red-500 font-medium">0</div>`,
+          );
+          increaseText.html(
+            `<div id="increase" className="text-red-500 font-medium">0% (+0)</div>`,
+          );
+
+          clickedBar = { id: null, color: null };
+        } else {
+          d3.select('#' + clickedBar.id)
+            .attr('fill', clickedBar.color)
+            .attr('stroke', clickedBar.color)
+            .attr('stroke-width', 0.3);
+          const claimsReceived = d.data[1];
+          receivedText.html(
+            `<div id="received" className="text-red-500 font-medium">${claimsReceived}</div>`,
+          );
+
+          const diff = parseInt(claimsReceived) - parseInt(claimsPreviousMonth);
+          const rate = (diff / claimsReceived) * 100;
+
+          if (clickedId !== 'mybar0') {
+            if (diff < 0) {
+              upOrDown.html('<span>Disminuyeron</span>');
+              increaseText.html(
+                `<div id="increase" className="text-red-500 font-medium">${
+                  Math.trunc(rate * 100) / 100
+                }% (${diff})</div>`,
+              );
+            } else {
+              upOrDown.html('<span>Aumentaron</span>');
+              increaseText.html(
+                `<div id="increase" className="text-red-500 font-medium">${
+                  Math.trunc(rate * 100) / 100
+                }% (+${diff})</div>`,
+              );
+            }
+          } else {
+            increaseText.html(
+              `<div id="increase" className="text-red-500 font-medium">${0}% (+${0})</div>`,
+            );
+          }
+
+          clickedBar = {
+            id: clickedId,
+            color: d3.select(e.currentTarget).attr('fill'),
+          };
+        }
+        if (clickedBar.id) {
+          d3.select(e.currentTarget)
+            .attr('fill', '#ff0000')
+            .attr('stroke', '#000000')
+            .attr('stroke-width', 0.5);
+        }
+      })
+      .style('cursor', 'pointer')
       .text((d) => MONTHS[d.data[0]])
       .attr('transform', (d) => `translate(${arc.centroid(d)})`)
       .attr('text-anchor', 'middle')
       .attr('font-size', '16px')
       .attr('fill', textColor);
 
-    data.forEach((d, i) => {
+    dataSorted.forEach((d, i) => {
       svg
         .append('circle')
         .attr('cx', -width / 2 + 20)
-        .attr('cy', i * 20 - data.length * 10)
+        .attr('cy', i * 20 - dataSorted.length * 10)
         .attr('r', 6)
         .style('fill', colorScale(i));
 
       svg
         .append('text')
         .attr('x', -width / 2 + 40)
-        .attr('y', i * 20 - data.length * 10)
+        .attr('y', i * 20 - dataSorted.length * 10)
         .text(`${MONTHS[d[0]] ?? d[0]}: ${d[1]}`)
         .style('font-size', '15px')
         .attr('alignment-baseline', 'middle');
